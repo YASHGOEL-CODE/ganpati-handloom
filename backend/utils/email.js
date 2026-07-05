836f3232697bc9dfdc220a831df9da38e312d4e2
Yash Goel <yashgoel2030@gmail.com>
2026-07-06 01:16:35 +0530

    fix: email verification working

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -8,7 +8,7 @@ const createTransporter = () => {
     secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
+      pass: process.env.EMAIL_PASS,
     },
   });
 };
